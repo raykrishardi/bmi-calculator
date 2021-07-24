06ed1126878387bfb90bd4fa64f1dc2c851a4bce
Ray Krishardi Layadi <raykrishardi@gmail.com>
2021-07-24 23:56:26 +1000

    Change default port to avoid conflict with Jenkins default port

--- src/config/config.js
+++ src/config/config.js
@@ -14,7 +14,7 @@ const config = convict({
   port: {
    doc: 'The port to bind depending on application environment',
    format: 'port',
-   default: 8080,
+   default: 9000,
    env: 'NODE_PORT',
    arg: 'nodePort'
   },
